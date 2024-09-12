a56a787d077af31734a19dde047cfef4fc4b6898
Student User <student@workstation.lab.example.com>
2024-09-12 04:23:51 -0400

    Refactor

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -6,5 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingCalculatorTest {
 
-    // @todo: add tests
+    @Test
+    public void canCalculateTheCostForARegion() throws RegionNotFoundException {
+        ShippingCalculator calculator = new ShippingCalculator();
+        assertEquals(0, calculator.costForRegion("A Region"));
+    
+    }
+
+    @Test
+    public void onNARegionTheCostIs100() throws RegionNotFoundException {
+
+        ShippingCalculator calculator = new ShippingCalculator();
+        int calculatedCost = calculator.costForRegion("NA");
+        assertEquals(100,calculatedCost);
+    }
+
+    @Test
+    public void onLATAMRegionTheCostIs200() throws RegionNotFoundException {
+
+        ShippingCalculator calculator = new ShippingCalculator();
+        int calculatedCost = calculator.costForRegion("LATAM");
+        assertEquals(200,calculatedCost);
+
+    }
+
+    @Test
+    public void onNonSupportedRegionARegionNotFoundExceptionIsRaised() {
+        ShippingCalculator calculator = new ShippingCalculator();
+        assertThrows(RegionNotFoundException.class,
+        () -> calculator.costForRegion("Unknown Region")
+        );
+    }
+    
 }
